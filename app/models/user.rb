53fc3c020ef5b5dc634f804df4e191f7916ffcad
Joe Webb & Jennifer Oseitutu <pair+imjoewebb.jennieo23@devbootcamp.com>
2014-08-08 15:04:55 -0500

    added test to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :resources, foreign_key: :creator_id
 
-  validates_presence_of :first_name, :last_name, :email, :username, :password_digest
-  validates :email, uniqueness: :true, format: {with: /\A.+@.+\.+.+{2,4}\z/, message: "Invalid email format"}
+  validates_presence_of :first_name, :cohort, :last_name, :email, :username, :password_digest
+  validates :email, uniqueness: { case_sensitive: false }, format: {with: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i, message: "Invalid email format"}
+  validates :password, length: { minimum: 5 }
   has_secure_password
 
 
