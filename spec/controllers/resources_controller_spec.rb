c43053f276a115f82be351038e8fbc2ea4e8a3a6
Joe Webb <imjoewebb@gmail.com>
2014-08-09 12:31:23 -0500

    fixed a typo

--- spec/controllers/resources_controller_spec.rb
+++ spec/controllers/resources_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe ResourcessController, :type => :controller do
+RSpec.describe ResourcesController, :type => :controller do
   describe "create" do
     it "should save the resource to the db if the form is valid" do
       post :create, resource: {title: "RSpec", link: "https://www.relishapp.com/rspec/rspec-expectations/v/2-0/docs/matchers/respond-to-matcher", description: "How to test respond-to-matcher" }
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,6 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 require 'database_cleaner'
 require 'capybara/rspec'
 require 'capybara/rails'
