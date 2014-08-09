0c4f7ed1da3c77899c97c5181c16c572459a007a
Joe Webb & Jennifer Oseitutu <pair+imjoewebb.jennieo23@devbootcamp.com>
2014-08-09 10:43:15 -0500

    added tests for resources model

---
+++ spec/controllers/resources_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe ResourcessController, :type => :controller do
+  describe "create" do
+    it "should save the resource to the db if the form is valid" do
+      post :create, resource: {title: "RSpec", link: "https://www.relishapp.com/rspec/rspec-expectations/v/2-0/docs/matchers/respond-to-matcher", description: "How to test respond-to-matcher" }
+      expect(response).to redirect_to Resource.find_by(title: "RSpec", link: "https://www.relishapp.com/rspec/rspec-expectations/v/2-0/docs/matchers/respond-to-matcher", description: "How to test respond-to-matcher")
+    end
+  end
+end
