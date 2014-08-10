a1cf5c9c5889d1a5fac4485feeddc8171f91ebd0
Joe Webb <imjoewebb@gmail.com>
2014-08-10 13:51:30 -0500

    modified seed file for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,5 @@
 require 'faker'
 
-User.delete_all
 LearningStyle.delete_all
 Subject.delete_all
 # This file should contain all the record creation needed to seed the database with its default values.
@@ -12,17 +11,4 @@ Subject.delete_all
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 LearningStyle.create([{name: 'Visual'}, {name: 'Auditory'}, {name: 'Read/Write'}, {name: 'Kinesthetic'}])
 
-
-users = 100.times.map do
-  User.create!( :first_name => Faker::Name.first_name,
-                :last_name  => Faker::Name.last_name,
-                :cohort => Faker::Name.last_name,
-                :email      => Faker::Internet.email,
-                :username => Faker::Name.last_name,
-                :password   => "password")
-end
-
-subjects =
-  Subject.create!([{name: "RSpec"}, {name: "CSS"}, {name: "Ruby"}, {name: "SQL"}, {name: "ActiveRecord"}, {name: "Rails"}, {name: "RSpec"}])
-
-resources =
+Subject.create!([{name: "RSpec"}, {name: "CSS"}, {name: "Ruby"}, {name: "SQL"}, {name: "ActiveRecord"}, {name: "Rails"}])
